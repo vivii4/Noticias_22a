195a63243ec3dc61cb3b55197ce9ac8dc37ee780
vivii4 <vitoriasilvag1011@gmail.com>
2024-03-11 00:24:09 -0300

    Criaçao da navegaçao DeskTop, remoçao da classe header.mobile e inicio da responsividade

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const botaoAbrir = document.querySelector(".header-mobile > button")
-const botaoFechar = document.querySelector(".header-mobile nav button")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 const body = document.querySelector("body")
-const nav = document.querySelector(".header-mobile nav")
+const nav = document.querySelector("header .mobile ")
 
 console.log(body)
 
